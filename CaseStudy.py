0726dab5377eae4706bb958e28784b5930cf2be8
carlotta90 <56724972+carlotta90@users.noreply.github.com>
2019-10-18 14:38:03 +0100

    Update CaseStudy.py

--- CaseStudy.py
+++ CaseStudy.py
@@ -9,8 +9,21 @@ Title: Case Study
 
 import numpy as np 
 
-np.random.seed(123) #initializes random numbers
+np.random.seed(123) # initializes random numbers
+random_walk = [0] # initializes the random_walk
 
-for i in 'casa' :
-    print(np.random.int(0,6))
-    
+for i in range(100) : # iterates 100 times
+    step = random_walk[-1] #set step to the last elements of random_walk
+    dice = np.random.randint(1,7) # rolls the dice
+
+    if dice <= 2 :
+        step -= 1
+    elif dice <= 5 :
+        step += 1
+    else :
+        step += np.random.randint(1,7)
+        
+    random_walk.append(step) # updates the last step
+
+
+print(random_walk)
