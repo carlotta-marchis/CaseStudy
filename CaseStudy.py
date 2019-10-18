7716d848f65f74b630b22b4af2100aef05752336
carlotta90 <56724972+carlotta90@users.noreply.github.com>
2019-10-18 14:08:34 +0100

    Add files via upload

---
+++ CaseStudy.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 18 13:59:48 2019
+
+@author: Carlotta
+
+Title: Case Study
+"""
+
+import numpy as np 
+
+np.random.seed(123) #initializes random numbers
+
+for i in 'casa' :
+    print(np.random.rand())
